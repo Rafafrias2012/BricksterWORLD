a22e90db0d084a87e95abc5b93bae1f00f735ab3
Rafafrias2012 <155685529+Rafafrias2012@users.noreply.github.com>
2024-07-17 10:52:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -16,8 +16,15 @@ io.on('connection', (socket) => {
     socket.broadcast.emit('remove', socket.id);
   });
 
+  socket.on('login', (nickname) => {
+    agents[socket.id] = { nickname, x: Math.floor(Math.random() * (window.innerWidth - 50)), y: Math.floor(Math.random() * (window.innerHeight - 50)) };
+    socket.emit('loginSuccess', agents[socket.id]);
+    socket.broadcast.emit('newUser', agents[socket.id]);
+  });
+
   socket.on('move', (data) => {
-    agents[socket.id] = data;
+    agents[socket.id].x = data.x;
+    agents[socket.id].y = data.y;
     socket.broadcast.emit('update', { id: socket.id, x: data.x, y: data.y });
   });
 });
